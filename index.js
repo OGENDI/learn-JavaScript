92ff5ac03bf4dbb4f68f94df0d9e6a27a7839d83
OGENDI <demokonge17@gmail.com>
2022-07-27 11:59:27 +0300

    Trying out to fix error

---
+++ index.js
@@ -0,0 +1,70 @@
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 7
+// let secondBatch = 5
+
+// let count = firstBatch +  secondBatch
+
+
+// console.log(count)
+
+// let myAge = 27
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+
+// console.log(myDogAge)
+
+// let bonusPoints = 50
+
+// bonusPoints += 50
+
+// console.log(bonusPoints)
+
+// bonusPoints -= 75
+
+// console.log(bonusPoints)
+
+// bonusPoints += 45
+
+// console.log(bonusPoints)
+
+
+// function increment() {
+//     console.log("You clicked the button!")
+
+// }
+
+// function myLogger() {
+//     console.log(42)
+// }
+
+// myLogger()
+
+// let lapsCompleted = 0
+
+// function incrementLap() {
+//     lapsCompleted += 1
+//     console.log(lapsCompleted)
+
+// }
+
+// incrementLap()
+// incrementLap()
+let count = 0
+
+function increment() {
+    count += 1
+    document.getElementById("count-el").innerText = count
+    console.log(count)
+}
+
+function save() {
+    console.log(count)
+}
+
+// let name1 = "Derick Ogendi"
+// let greeting = "Welcome back "
+
+// document.getElementById("welcome-el").innerText = greeting + name1
