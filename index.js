20807d292eaf7d4c3a6ce36d3296c9a95d208698
OGENDI <demokonge17@gmail.com>
2022-07-27 13:17:12 +0300

    Simple station count app

--- index.js
+++ index.js
@@ -56,13 +56,18 @@ let count = 0
 
 function increment() {
     count += 1
-    document.getElementById("count-el").innerText = count
+    document.getElementById("count-el").textContent = count
     console.log(count)
 }
 
 function save() {
+    let countStr = count + ' - '
+
+    document.getElementById("save-el").textContent += countStr
     console.log(count)
-    
+    document.getElementById("count-el").textContent = 0
+    count = 0
+
 }
 
 // let name1 = "Derick Ogendi"
